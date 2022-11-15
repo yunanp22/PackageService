85eacbffd35516df3d4141d5fa35fc01139ba09d
Leebumju <dlqkawns@naver.com>
2022-11-15 14:13:56 +0900

    Initial Commit

---
+++ PackageService/PackageServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackageServiceApp.swift
+//  PackageService
+//
+//  Created by 이범준 on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct PackageServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
