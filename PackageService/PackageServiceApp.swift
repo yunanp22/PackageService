6363a2daa31675c7247382c0add97cac16c87d18
Leebumju <dlqkawns@naver.com>
2022-11-16 19:17:33 +0900

    파이어베이스 연동 완료

--- PackageService/PackageServiceApp.swift
+++ PackageService/PackageServiceApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PackageServiceApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
