796d4d72b76b9609dad714fbacfb602b333f4f33
Leebumju <dlqkawns@naver.com>
2022-11-18 21:53:34 +0900

    Test4

--- PackageService/ContentView.swift
+++ PackageService/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.red)
-            Text("Hello????????")
+            Text("Hello??????!!!!??")
         }
         .padding()
     }
