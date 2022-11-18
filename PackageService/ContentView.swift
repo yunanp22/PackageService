01e1a02c328d8d6c956762b16baa56e069531b4a
yunan <yunanp22@gmail.com>
2022-11-18 21:10:00 +0900

    test

--- PackageService/ContentView.swift
+++ PackageService/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.red)
             Text("Hello, world!")
         }
         .padding()
