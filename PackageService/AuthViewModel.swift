8eb655ac81ffbb64831a6576721d999f88943292
Leebumju <dlqkawns@naver.com>
2022-11-23 19:18:16 +0900

    파이어 베이스, 화면 연동

--- PackageService/AuthViewModel.swift
+++ PackageService/AuthViewModel.swift
@@ -14,7 +14,7 @@ class AuthViewModel: ObservableObject {
         
     }
     func registerUser(_ id: String, _ password: String, _ nickName: String, _ address: String, _ email: String, _ name: String) {
-        Auth.auth().createUser(id: id, password: password, nickName: nickName, address: address, email: email, name: name) { result, error in
+        Auth.auth().createUser(withEmail: email, password: password)  { result, error in
             if let error = error {
                 print("Error : \(error.localizedDescription)")
                 return
